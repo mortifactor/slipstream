208c1ca9176d4db958e7e0c41f03f891a1a401a6
Jop Zitman <jop-zitman@hotmail.com>
2025-01-23 02:08:33 +0800

    fix

--- include/slipstream.h
+++ include/slipstream.h
@@ -17,10 +17,11 @@ extern "C" {
 #define SLIPSTREAM_CLIENT_TICKET_STORE "sample_ticket_store.bin";
 #define SLIPSTREAM_CLIENT_TOKEN_STORE "sample_token_store.bin";
 #define SLIPSTREAM_QLOG_DIR "./qlog";
+#include <stdbool.h>
 
 
 int picoquic_slipstream_client(int listen_port, char const* resolver_addresses_filename, const char* domain_name,
-                               const char* cc_algo_id);
+                               const char* cc_algo_id, bool gso);
 
 int picoquic_slipstream_server(int server_port, const char* pem_cert, const char* pem_key, char const* upstream_name,
                                int upstream_port, const char* domain_name);
